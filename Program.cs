00d6945ba65173f911305db4c7b0fb9b6dc8d473
Jordan-Cox92! <mrjordancolbycox@gmail.com>
2022-11-17 10:32:24 -0500

    first c-sharp

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿
+using System;
+//same as importing in java
+
+Console.Write("Who would you like to say hello to? ");
+
+string name = Console.ReadLine();
+
+if (string.IsNullOrWhiteSpace(name))
+{
+    Console.WriteLine("Fine, don't say 'hello'!");
+}
+else
+{
+    Console.WriteLine($"Hello, {name}!");
+}
+
+
+
+
+
